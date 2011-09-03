f4509e3e47bae83a462f7a94d04829b8ec67085e
Pacharavalai <pach2307@yahoo.com>
2011-09-03 10:50:41 +0700

    First Commit
    Windiw Bas Template

---
+++ DummyPrj/DummyPrjAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DummyPrjAppDelegate.h
+//  DummyPrj
+//
+//  Created by Apple on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyPrjAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
