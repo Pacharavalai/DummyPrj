7d59047eeb4a8ee3483c3dd382d31ea29ea72768
Pacharavalai <pach2307@yahoo.com>
2011-09-03 11:02:05 +0700

    Modify

--- DummyPrj/DummyPrjAppDelegate.h
+++ DummyPrj/DummyPrjAppDelegate.h
@@ -6,11 +6,16 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+
+// AAA
+// BBB
+
 #import <UIKit/UIKit.h>
 
 @interface DummyPrjAppDelegate : NSObject <UIApplicationDelegate> {
 
 }
+
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 
